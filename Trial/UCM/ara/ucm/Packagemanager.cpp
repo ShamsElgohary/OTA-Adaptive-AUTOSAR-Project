abc3a9eb86bc93dc65a45015c322d256f3fb42cb
Shams Elgohary <shamselgohary17@gmail.com>
2021-11-26 18:11:39 +0200

    PackageManagerImpl Implementation

--- Trial/UCM/ara/ucm/Packagemanager.cpp
+++ Trial/UCM/ara/ucm/Packagemanager.cpp
@@ -1,6 +1,75 @@
 #include "includes/Packagemanager.hpp"
 
 
-using json = nlohmann::json;
 using namespace ara::ucm::pkgmgr;
 using namespace std;
+
+
+ara::ucm::TransferStartReturnType PackageManagerImpl::TransferStart(uint64_t Size)
+{
+    ara::ucm::TransferStartReturnType StartReturn =  ara::ucm::transfer::SoftwarePackage::TransferStart(Size);
+    return StartReturn;
+}
+
+ara::ucm::OperationResultType  PackageManagerImpl::TransferData(TransferIdType &id, ByteVectorType data, uint64_t blockCounter)
+{
+    ara::ucm::OperationResultType DataReturn =  ara::ucm::transfer::SoftwarePackage::TransferData(id, data, blockCounter);
+    return DataReturn;
+}
+
+ara::ucm::OperationResultType PackageManagerImpl::TransferExit(TransferIdType &id)
+{
+    ara::ucm::OperationResultType ExitReturn =  ara::ucm::transfer::SoftwarePackage::TransferExit(id);
+    return ExitReturn;
+}
+
+
+void PackageManagerImpl::ProcessSwPackage(TransferIdType &id)
+{
+    string StringID { Convert2StringID(id) };
+
+    /* GET PACKAGE */
+    std::string SWPackagePath{ZIP_PackagesPath + "TransferID:" + StringID + ".zip"} ;
+
+    /* USED TO PARSE */
+    ara::ucm::parsing::SoftwarePackageParser SWParser_instance;
+
+    SWParser_instance.UnzipFile(SWPackagePath);
+    
+    SWParser_instance.SwPackageManifestParser(SWPackagePath);
+
+    SwClusterInfoType NewSwClusterInfo {SWParser_instance.GetSwClusterInfo(SWPackagePath)};
+
+    ActionType action { SWParser_instance.GetActionType() };
+
+    /* CHECK ACTION TYPE AND ACT ACCORDING TO IT */
+    if ( action == ActionType::kUpdate )
+    {
+        // UpdateInstance should have a constructor that takes PackagePath 
+        shared_ptr<storage::UpdateAction> updateInstance;
+        ara::ucm::storage::SWCLManager::AddSWCLChangeInfo(NewSwClusterInfo , updateInstance);
+        updateInstance->Execute();
+    }
+
+    else if ( action == ActionType::kInstall )
+    {
+        shared_ptr<storage::InstallAction> installInstance;
+        ara::ucm::storage::SWCLManager::AddSWCLChangeInfo(NewSwClusterInfo , installInstance);
+        installInstance->Execute();
+    }
+    
+    else if ( action == ActionType::kRemove )
+    {
+        shared_ptr<storage::InstallAction> removeInstance;
+        ara::ucm::storage::SWCLManager::AddSWCLChangeInfo(NewSwClusterInfo , removeInstance);
+        removeInstance->Execute();
+    }
+
+    else
+    {
+        // WRONG ACTION TYPE
+    }
+
+    return;
+}
+
--- Trial/UCM/ara/ucm/includes/Packagemanager.hpp
+++ Trial/UCM/ara/ucm/includes/Packagemanager.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
+#include "Transfer.hpp"
+#include "Parsing.hpp"
+#include "Storage.hpp"
 #include "UCM_Common.hpp"
+
 
 namespace ara
 {
@@ -8,11 +12,23 @@ namespace ara
 	{
 		namespace pkgmgr
 		{
-			class PackageManager {
+			class PackageManagerImpl {
 			private:
-			
+			/* The current status of UCM */
+			static PackageManagerStatusType CurrentStatus;
 
 			public:
+
+			ara::ucm::TransferStartReturnType TransferStart(uint64_t Size);
+
+			ara::ucm::OperationResultType  TransferData(TransferIdType &id, ByteVectorType data, uint64_t blockCounter);
+			
+			ara::ucm::OperationResultType TransferExit(TransferIdType &id);
+
+			/* Process a previously transferred Software Package */
+			void ProcessSwPackage(TransferIdType &id);
+
+
 
 
 
