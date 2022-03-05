652114c23fded4f0dd639bcc17254f4fa41e714a
YoussefAbaas <76699316+YoussefAbaas@users.noreply.github.com>
2022-03-05 06:22:21 +0200

    Update proxy_test.cpp

--- ara/com/proxy_test.cpp
+++ ara/com/proxy_test.cpp
@@ -1,14 +1,18 @@
 #include "Internal/UpdateRequestProxy.hpp"
+#include "memory"
 using namespace ara::com::proxy;
 int main()
 {
     // some code to acquire the handle 
-    //...
-    ara::com::internal::Proxy::HandleType handle;
-    UpdateRequestProxy proxy{handle};
+    std::unique_ptr<UpdateRequestProxy>proxy;
+    auto handles=UpdateRequestProxy::FindService();
+    if(!handles.empty())
+    {
+        proxy=std::make_unique<UpdateRequestProxy>(handles[0]);
+    }
     //call function
     std::future<methods::StartUpdateSessionOutput>StartUpdateSession_Call_Future;
-    StartUpdateSession_Call_Future=proxy.StartUpdateSession();
+    StartUpdateSession_Call_Future=proxy->StartUpdateSession();
     // now we do a blocking get() which will return in case of result has set
     methods::StartUpdateSessionOutput StartUpdateSession_Call_Output=StartUpdateSession_Call_Future.get();
     return 0;
