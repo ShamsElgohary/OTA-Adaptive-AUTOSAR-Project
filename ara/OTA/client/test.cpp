218ec5ad5b93568bd617439803bed60426b2609e
karim-hafez77 <76133282+karim-hafez77@users.noreply.github.com>
2022-03-09 22:37:04 +0200

    parsing meta data

--- ara/OTA/client/test.cpp
+++ ara/OTA/client/test.cpp
@@ -1,12 +1,58 @@
 #include "cpr/cpr.h"
 #include "iostream"
 #include "fstream"
+#include <string>
+#include <vector>
+using namespace std;
+struct meta_data
+{
+    string name;
+    int version;
+};
+
+
+void print(meta_data x){
+    cout<<"name= "<<x.name<<'\n';
+    cout<<"version= "<<x.version<<'\n';
+}
+
+void parse_meta_data(){
+    vector <meta_data> trail;
+    ifstream myfile; myfile.open("meta_data.txt");
+    string myline;
+
+    while ( myfile ) { 
+            meta_data x;
+            getline (myfile, myline);
+            if (myline == "")
+            {
+            }
+            else{
+            int it=myline.find("#");
+            string file_name=myline.substr(0,it);
+            string number=myline.substr(it+1);
+            int num=stoi(number);
+            x.name=file_name;
+            x.version=num;
+            trail.push_back(x);
+            }
+        } 
+
+    for(int i=0; i < trail.size(); i++)
+    {
+    print(trail[i]);
+    }
+
+}
+
+
 int main()
 {
     auto responce=cpr::Get(cpr::Url{"http://127.0.0.1:8000/meta_data_send"});
     std::cout<<responce.text<<std::endl;
     std::ofstream myfile;
-    myfile.open("all.txt");
+    myfile.open("meta_data.txt");
     myfile<<responce.text;
     myfile.close();
+    parse_meta_data();
 }
--- ara/OTA/cloud/cloud/cloud/core/models.py
+++ ara/OTA/cloud/cloud/cloud/core/models.py
@@ -13,7 +13,7 @@ def file_path(instance,filename):
 class FileHandler(models.Model):
    file_upload=models.FileField(upload_to=file_path,blank=True, null=True)
    file_name=models.CharField(null=False,max_length=500,default='')
-   file_version=models.FloatField()
+   file_version=models.FloatField(default='')
 
    def __str__(self) :
       return self.file_name
