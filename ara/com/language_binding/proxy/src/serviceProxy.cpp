c317fcd751f38ff385b7ca887a0a59cb208f556e
loayElkholy <40799479+loayElkholy@users.noreply.github.com>
2022-04-22 01:43:10 +0200

    handle fix

--- ara/com/language_binding/proxy/src/serviceProxy.cpp
+++ ara/com/language_binding/proxy/src/serviceProxy.cpp
@@ -33,12 +33,14 @@ namespace ara
 
                 if (parse("service_manifest.json", instanceId) == "SOME/IP")
                 {
+                    cout<<instanceId<<endl ;
                     vector<serviceinfo> opVsomeip = ara::com::SomeIpNetworkBinding::FindService_SomeIp(serviceID, instanceId);
                     for (uint8_t i = 0; i < opVsomeip.size(); i++)
                     {
                         ProxyBase::HandleType h;
                         h.InstanceID = opVsomeip[i].Instance_ID;
                         h.ptr2bindingProtocol = std::make_shared<SomeIpNetworkBinding>(opVsomeip[i].ipv4_address, opVsomeip[i].port_num);
+                        cout<<h.InstanceID<<" "<<typeid(h.ptr2bindingProtocol).name()<<endl;
                         vectorOfHandles.push_back(h);
                     }
                     
