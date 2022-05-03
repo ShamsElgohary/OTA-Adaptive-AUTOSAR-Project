7dbde7b48851f14e729c99d1e35eb4d511a93529
YoussefAbaas <76699316+YoussefAbaas@users.noreply.github.com>
2022-05-04 00:51:36 +0200

    File_Hash_CryptoTest

--- ara/crypto/cryp/src/test_main.cpp
+++ ara/crypto/cryp/src/test_main.cpp
@@ -1,23 +1,34 @@
 #include "../include/crypto_provider.hpp"
+#include <fstream>
+#include <cmath>
+#include <vector>
 #include <iostream>
 #include <string>
 using namespace CryptoPP;
-using namespace ara::crypto::cryp;
+using namespace ara::crypto;
+using namespace std;
 
 int main()
 {
-
-    std::string msg = "Hello";
-    std::string digest;
-
-    std::vector <byte> Digest;
-
-    CryptoProvider cryp_provider;
-    HashFunctionCtx::Uptr hash = cryp_provider.CreateHashFunctionCtx(HashCtx_AlgID::SHA224_ID);
-    hash->Start();
-    hash->Update((const byte*)msg.data(), msg.length());
+    std::string path =  "/home/youssef/Documents/GitHub/OTA-Adaptive-AUTOSAR-Project/ara/crypto/cryp/src/test1.txt";
     
-    std::cout << "Message: " << msg << std::endl;
+    ifstream ifs(path, ios::binary | ios::ate);
+    ifstream::pos_type pos = ifs.tellg();
+    std::vector<char> result(pos);
+    ifs.seekg(0, ios::beg);
+    ifs.read(&result[0], pos);
+    
+    char* result_arr=nullptr;
+    result_arr=&result[0];
+
+    std::vector <unsigned char> Digest;
+
+    cryp::CryptoProvider cryp_provider;
+    cryp::HashFunctionCtx::Uptr hash = cryp_provider.CreateHashFunctionCtx(cryp::HashCtx_AlgID::SHA1_ID);
+    hash->Start();
+    hash->Update(ReadOnlyMemRegion(result_arr), result.size());
+    
+    //std::cout << "Message: " << msg << std::endl;
 
     std::cout << "Digest: ";
     std::cout << std::endl;
