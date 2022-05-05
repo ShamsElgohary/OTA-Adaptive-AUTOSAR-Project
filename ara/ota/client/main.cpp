e4ec79bf8f83ec249e44c989cc3196a5e1c32ffd
YasserAhmedMoh <48395651+YasserAhmedMoh@users.noreply.github.com>
2022-05-05 21:01:25 +0200

    crypto and ota integration
    Co-Authored-By: YoussefAbaas <76699316+YoussefAbaas@users.noreply.github.com>

--- ara/ota/client/main.cpp
+++ ara/ota/client/main.cpp
@@ -1,23 +1,30 @@
 #include "cpr/cpr.h"
 #include "iostream"
+#include <filesystem>
+#include "../../crypto/cryp/include/crypto_provider.hpp"
 #include "PackageManagerProxy.hpp"
 #include "execution_client.hpp"
 #include "fstream"
 #include <string>
 #include <vector>
+using std::filesystem::current_path;
 using namespace std;
 using namespace ara::ucm::pkgmgr::proxy;
 using namespace ara::com::proxy;
-using namespace std;
+using namespace ara::crypto;
+
 
 class CLIENT_OTA
 {
+    
 private:
     vector<SwClusterInfoType> trail;
     vector<SwClusterInfoType> UCM_trail;
     PackageManagerProxy *RecService = nullptr;
+    
 
 public:
+    
     CLIENT_OTA()
     {
         ara::com::ServiceHandleContainer<ProxyBase::HandleType> handles = PackageManagerProxy::FindService();
@@ -28,6 +35,76 @@ public:
         else
         {
             cout<<"NO UCM";
+        }
+    }
+    
+    string hash(string packagename)
+    {
+        //string filename = packagename + ".zip";
+        string url_final = "http://127.0.0.1:8000/hash/" + packagename + ".zip";
+        auto responce = cpr::Get(cpr::Url{url_final});
+        // std::cout<<responce.text<<std::endl;
+        return responce.text;
+    }
+
+    string crypto_get_hash(string packagename)
+    {
+        std::string path =  "/home/yasser/Documents/GitHub/OTA-Adaptive-AUTOSAR-Project/ara/ota/client/ex1.zip";
+    
+        ifstream ifs(path, ios::binary | ios::ate);
+        ifstream::pos_type pos = ifs.tellg();
+        std::vector<char> result(pos);
+        ifs.seekg(0, ios::beg);
+        ifs.read(&result[0], pos);
+        
+        char* result_arr=nullptr;
+        result_arr=&result[0];
+
+        std::vector <unsigned char> Digest;
+
+        cryp::CryptoProvider cryp_provider;
+        cryp::HashFunctionCtx::Uptr hash = cryp_provider.CreateHashFunctionCtx(cryp::HashCtx_AlgID::SHA1_ID);
+        hash->Start();
+        hash->Update(ReadOnlyMemRegion(result_arr), result.size());
+        
+        //std::cout << "Message: " << msg << std::endl;
+
+        Digest=hash->Finish();
+        
+        std::stringstream ss;
+        
+        for (int i = 0; i < Digest.size(); i++)
+        {
+            //std::cout << static_cast<unsigned>(Digest[i]) << "    ";
+            if(Digest[i]>=0 && Digest[i] <= 15)
+            {
+                ss<<"0"<< std::hex<<int(Digest[i]);
+            }
+            else
+            {
+                ss<< std::hex<<int(Digest[i]);
+            }
+            
+            
+            
+        }
+
+        std::string res(ss.str());
+        
+        return res;
+
+
+    }
+    bool compare_hash(string packagename)
+    {
+        std::string hash_server= hash(packagename);
+        std::string hash_cryp=crypto_get_hash(packagename);
+        //cout << hash_server<< endl;
+        //cout << hash_cryp << endl;
+        if(hash_server == hash_cryp)
+        {return 1;}
+        else{
+            return 0;
         }
     }
 
@@ -57,6 +134,8 @@ public:
             }
         }
     }
+    
+
     void get_meta_data()
     {
         string url_final = "https://coretestsystem.herokuapp.com/meta_data_send";
@@ -128,9 +207,17 @@ public:
                     }
                     else
                     {
+                        
                         string pckg = cluster.Name + "#" + cluster.Version;
+                        if(compare_hash(pckg))
+                        {
                         download(pckg);
                         this->Transfer2UCM(pckg);
+                        }
+                        else
+                        {
+                            cout << "Error: Different hash "
+                        }
                     }
                 }
             }
@@ -153,11 +240,17 @@ public:
         cout << "3" << endl;
         Compare();
     }
+    
+    
 };
 
 int main()
 {
     CLIENT_OTA x;
     x.run();
+    
+
+    
+    
     return 0;
 }
