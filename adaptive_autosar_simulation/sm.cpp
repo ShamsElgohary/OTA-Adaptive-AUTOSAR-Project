0a8e47ccdff40e1b058cc39e211421b2013cc81e
YasserAhmedMoh <48395651+YasserAhmedMoh@users.noreply.github.com>
2022-05-10 00:08:17 +0200

    SM_GUI

--- adaptive_autosar_simulation/sm.cpp
+++ adaptive_autosar_simulation/sm.cpp
@@ -3,4 +3,40 @@
 sm::sm(QWidget *parent): QWidget{parent}
 {
 
+    main_layout->addWidget(state_management, 0, 0, 2, 1);
+    main_layout->addWidget(console, 2, 0, 2, 1);
+
+
+
+    state_management->setLayout(new QVBoxLayout);
+
+    label->setText("Start Upadte Session");
+    state_management->layout()->addWidget(label);
+
+    state_management->layout()->addWidget(slider);
+    slider->setLayout(new QVBoxLayout());
+
+    slider->layout()->addWidget(s_slider);
+
+    p_slider->setStyleSheet("value: 20;");
+    slider->layout()->addWidget(p_slider);
+
+    p_slider->setValue(s_slider->value());
+    connect(s_slider, SIGNAL(valueChanged(int)), p_slider, SLOT(setValue(int)));
+
+    state_management->layout()->addWidget(function_group_state);
+    function_group_state->setLayout(new QVBoxLayout());
+    function_group_state->layout()->addWidget(function_group_state_table);
+    function_group_state_table->setRowCount(2);
+    function_group_state_table->setColumnCount(2);
+
+    start_simulation->setText("start simulation");
+    state_management->layout()->addWidget(start_simulation);
+
+    console->setLayout(new QVBoxLayout());
+    main_layout->addWidget(console, 2, 0, 2, 1);
+
+    console->layout()->addWidget(console_text);
+
+    setLayout(main_layout);
 }
--- adaptive_autosar_simulation/sm.h
+++ adaptive_autosar_simulation/sm.h
@@ -2,12 +2,32 @@
 #define SM_H
 
 #include <QWidget>
+#include <QtWidgets>
 
 class sm: public QWidget
 {
     Q_OBJECT
 public:
     explicit sm(QWidget *parent = nullptr);
+signals:
+private:
+
+    QGridLayout *main_layout = new QGridLayout;
+    QGroupBox *state_management = new QGroupBox("State Management", this);
+    QGroupBox *function_group_state = new QGroupBox("Function Group State", this);
+    QGroupBox *console = new QGroupBox("Console", this);
+    QGroupBox *slider = new QGroupBox("slider", this);
+    QPushButton * start_simulation =new QPushButton;
+    QTextBrowser *console_text = new QTextBrowser;
+    QLabel *label = new QLabel;
+    QSlider *s_slider = new QSlider(Qt::Horizontal, this);
+    QProgressBar *p_slider = new QProgressBar;
+    QTableWidget *function_group_state_table = new QTableWidget;
+
+
+
+
+
 };
 
 #endif // SM_H
