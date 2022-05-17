ca5c5b4b1e900d6570b193d80cb01355ca431626
karim-hafez77 <76133282+karim-hafez77@users.noreply.github.com>
2022-05-18 00:08:19 +0200

    socket change

--- ara/com/network_binding/SomeIp/servicediscovery/lib/include/servicestorage.hpp
+++ ara/com/network_binding/SomeIp/servicediscovery/lib/include/servicestorage.hpp
@@ -21,9 +21,6 @@
 #include <boost/filesystem.hpp>
 #include "../../../../../../../utility/jsoncpp/header/json.h"
 #include <string>
-void addtoGUI(uint16_t Service_ID, serviceinfo serviceinformation);
-void Addtorequests(uint16_t Service_ID, uint16_t instance_id);
-void addmsgtoGUI(uint16_t Service_ID,uint16_t instance_id,uint32_t ttl,uint16_t TYPE,uint16_t port_num, string ipv4_address);
 
 
 struct serviceinfo{
@@ -33,6 +30,7 @@ struct serviceinfo{
 };
 static std::map<uint16_t , std::vector<serviceinfo>> servicemap;
 
+
 class servicestorage{
 
 public:
@@ -41,3 +39,9 @@ static void RemoveService(uint16_t Service_ID,uint16_t instance_id);//stop offer
 static std::vector<serviceinfo> SearchServiceRegistry(uint16_t Service_ID,uint16_t Instance_ID);
 static serviceinfo SetServiceInfo(uint16_t Instance_ID,std::string ipv4_address,uint16_t port_num);
 };
+
+
+
+void addtoGUI(uint16_t Service_ID, serviceinfo serviceinformation);
+void Addtorequests(uint16_t Service_ID, uint16_t instance_id);
+void addmsgtoGUI(uint16_t Service_ID,uint16_t instance_id,uint32_t ttl,uint16_t TYPE,uint16_t port_num, string ipv4_address);
