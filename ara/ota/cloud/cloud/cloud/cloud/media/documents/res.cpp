e24e99fbd05fff9b0a1eabee586eded4f348a460
YasserAhmedMoh <48395651+YasserAhmedMoh@users.noreply.github.com>
2022-07-08 01:16:11 +0200

    cloud

--- ara/ota/cloud/cloud/cloud/cloud/media/documents/res.cpp
+++
@@ -1,8 +0,0 @@
-
-#include <iostream>
-#include <cpr.h>
-
-int main(int argc, char** argv) {
-    auto response = cpr::Get(cpr::Url{"http://127.0.0.1:8000/download/"});
-    std::cout << response.text << std::endl;
-}
