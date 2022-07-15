ce887c306ee6e9a09b5f89b973807214231aedd5
loayElkholy <40799479+loayElkholy@users.noreply.github.com>
2022-07-16 01:07:24 +0200

    Revert "Merge branch 'COM_Ideas_Fixed' of https://github.com/ShamsElgohary/OTA-Adaptive-AUTOSAR-Project into COM_Ideas_Fixed"
    This reverts commit b47b64d5f525d3d08a81d507a53c6eff2ba77e5c, reversing
    changes made to d249d7f2ffb58c1cd50a89431242d9dfeb4f7c7f.

--- requirments.sh
+++ requirments.sh
@@ -1,13 +1,6 @@
 
 #!/bin/sh
 
-sudo apt-get install libboost-all-dev
-
-sudo apt-get install nlohmann-json3-dev
-
-sudo apt-get install libcrypto++-dev libcrypto++-doc libcrypto++-utils
-
-cd ..
 sudo apt-get install libssl-dev
 sudo ldconfig -v
 git clone https://github.com/whoshuu/cpr.git
@@ -16,3 +9,7 @@ mkdir build && cd build
 cmake ..
 make
 sudo make install
+
+sudo apt-get install libboost-all-dev
+
+sudo apt-get install nlohmann-json3-dev
